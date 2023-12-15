f3b653fbf74dfdfa811c3299c7b71e0298768c7c
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-12-16 04:42:13 +0700

    Huy update

--- src/main/java/com/nvhuy/Application.java
+++ src/main/java/com/nvhuy/Application.java
@@ -1,7 +1,24 @@
 package com.nvhuy;
 
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Application {
     public static void main(String[] args) {
-
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(1);
+        priorityQueue.add(2);
+        priorityQueue.add(3);
+        priorityQueue.add(4);
+        priorityQueue.add(5);
+        priorityQueue.add(8);
+        priorityQueue.add(7);
+        priorityQueue.add(6);
+        System.out.println(priorityQueue);
+        priorityQueue.poll();
+        System.out.println(priorityQueue);
     }
 }
--- src/main/java/com/nvhuy/test/ProductTest.java
+++ src/main/java/com/nvhuy/test/ProductTest.java
@@ -9,22 +9,22 @@ import java.util.List;
 public class ProductTest {
     public static void main(String[] args) {
         ProductServiceImpl service = new ProductServiceImpl();
-        List<Product> productList = service.getAllProducts();
-        System.err.println(productList);
-        Product product = service.getProductById(56L);
-        System.err.println(product);
-
-        Product productSave = new Product("iphone11", 1000);
-        service.saveProduct(productSave);
-        List<Product> list = new ArrayList<>();
-        list.add(new Product("iphoneX1", 2000));
-        list.add(new Product("iphoneX2", 2000));
-        service.saveAllProducts(list);
-        Product updateProduct = new Product("Samsung");
-        service.updateProduct(4L, updateProduct);
-        Long id = 57L;
+//        List<Product> productList = service.getAllProducts();
+//        System.err.println(productList);
+//        Product product = service.getProductById(56L);
+//        System.err.println(product);
+//
+//        Product productSave = new Product("iphone11", 1000);
+//        service.saveProduct(productSave);
+//        List<Product> list = new ArrayList<>();
+//        list.add(new Product("iphoneX1", 2000));
+//        list.add(new Product("iphoneX2", 2000));
+//        service.saveAllProducts(list);
+//        Product updateProduct = new Product("Samsung");
+//        service.updateProduct(4L, updateProduct);
+        Long id = 45L;
         service.deleteProductById(id);
-        List<Product> productListLimit = service.getAllProducts(3, 3);
-        System.err.println(productListLimit);
+//        List<Product> productListLimit = service.getAllProducts(3, 3);
+//        System.err.println(productListLimit);
     }
 }
